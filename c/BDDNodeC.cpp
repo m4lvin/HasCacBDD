7a0c0f48ebb8cf441f47c7ff32b0a75a565852e6
Malvin Gattinger <malvin@w4eg.de>
2015-04-08 15:20:48 +0200

    first version of the c wrapper

---
+++ c/BDDNodeC.cpp
@@ -0,0 +1,79 @@
+#include "BDDNodeC.h"
+
+#include "BDDNode.h"
+
+BDD* BDD_new_XManagerPDD(XManager* m, DD vNode) { return new BDD(m, vNode); }
+
+BDD* BDD_new_BDDP(const BDD* right) { return new BDD(*right); }
+
+BDD* BDD_new() { return new BDD(); }
+
+void BDD_delete(BDD* this_ptr) { delete this_ptr; }
+
+int BDD_Variable(BDD* this_ptr) { return this_ptr->Variable(); }
+
+BDD BDD_Then(const BDD* this_ptr) { return this_ptr->Then(); }
+
+BDD BDD_Else(const BDD* this_ptr) { return this_ptr->Else(); }
+
+BDD BDD_Exist(BDD* this_ptr, const BDD* cube) { return this_ptr->Exist(*cube); }
+
+BDD BDD_Universal(BDD* this_ptr, const BDD* cube) { return this_ptr->Universal(*cube); }
+
+BDD BDD_Restrict(const BDD* this_ptr, const BDD* other) { return this_ptr->Restrict(*other); }
+
+BDD BDD_Compose(const BDD* this_ptr, int v, const BDD* other) { return this_ptr->Compose(v, *other); }
+
+BDD BDD_Permute(const BDD* this_ptr, const vector<int>* permu) { return this_ptr->Permute(*permu); }
+
+BDD BDD_AndExist(BDD* this_ptr, const BDD* other, const BDD* cube) { return this_ptr->AndExist(*other, *cube); }
+
+bool BDD_IsComp(BDD* this_ptr) { return this_ptr->IsComp(); }
+
+void BDD_Support(BDD* this_ptr, vector<int>* vars) { this_ptr->Support(*vars); }
+
+const XManager* BDD_manager(const BDD* this_ptr) { return this_ptr->manager(); }
+
+DD BDD_Node(const BDD* this_ptr) { return this_ptr->Node(); }
+
+XBDDManager* XBDDManager_new(int varCount) { return new XBDDManager(varCount); }
+
+void XBDDManager_delete(XBDDManager* this_ptr) { delete this_ptr; }
+
+void XBDDManager_Clear(XBDDManager* this_ptr, int varCnt) { this_ptr->Clear(varCnt); }
+
+BDD XBDDManager_BddOne(XBDDManager* this_ptr) { return this_ptr->BddOne(); }
+
+BDD XBDDManager_BddZero(XBDDManager* this_ptr) { return this_ptr->BddZero(); }
+
+BDD XBDDManager_BddVar(XBDDManager* this_ptr, int varIndex) { return this_ptr->BddVar(varIndex); }
+
+BDD XBDDManager_Ite(XBDDManager* this_ptr, const BDD* f, const BDD* g, const BDD* h) { return this_ptr->Ite(*f, *g, *h); }
+
+const XManager* XBDDManager_manager(const XBDDManager* this_ptr) { return this_ptr->manager(); }
+
+void XBDDManager_ShowInfo(XBDDManager* this_ptr, double vtime) { this_ptr->ShowInfo(vtime); }
+
+BDD BDD_Operator_Not(const BDD* this_ptr) { return (BDD) (! *this_ptr); }
+
+bool BDD_Operator_Equal(const BDD* this_ptr, const BDD* other) { return(bool) (*this_ptr == *other); }
+
+BDD BDD_Operator_Or(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr + *other); }
+
+BDD BDD_Operator_And(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr * *other); }
+
+BDD BDD_Operator_Xor(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr ^ *other); }
+
+BDD BDD_Operator_LessThan(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr < *other); }
+
+BDD BDD_Operator_MoreThan(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr > *other); }
+
+BDD BDD_Operator_LessEqual(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr <= *other); }
+
+BDD BDD_Operator_MoreEqual(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr >= *other); }
+
+BDD BDD_Operator_Nor(const BDD* this_ptr, const BDD* other) { return (BDD) (*this_ptr % *other); }
+
+BDD BDD_Operator_Nand(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr | *other); }
+
+BDD BDD_Operator_XNor(const BDD* this_ptr, const BDD* other) { return(BDD) (*this_ptr & *other); }
---
+++ c/BDDNodeC.h
@@ -0,0 +1,51 @@
+#include <BDDNode.h>
+
+extern "C" {
+
+BDD* BDD_new();
+BDD* BDD_new_XManagerPDD(XManager* m, DD vNode);
+BDD* BDD_new_BDDP(const BDD* right);
+void BDD_delete(BDD* this_ptr);
+const XManager* BDD_checkSameManager(const BDD* this_ptr, const BDD* other);
+int BDD_Variable(BDD* this_ptr);
+BDD BDD_Then(const BDD* this_ptr);
+BDD BDD_Else(const BDD* this_ptr);
+BDD BDD_Exist(BDD* this_ptr, const BDD* cube);
+BDD BDD_Universal(BDD* this_ptr, const BDD* cube);
+BDD BDD_Restrict(const BDD* this_ptr, const BDD* other);
+BDD BDD_Compose(const BDD* this_ptr, int v, const BDD* other);
+BDD BDD_Permute(const BDD* this_ptr, const vector<int>* permu);
+BDD BDD_AndExist(BDD* this_ptr, const BDD* other, const BDD* cube);
+bool BDD_IsComp(BDD* this_ptr);
+void BDD_Support(BDD* this_ptr, vector<int>* vars);
+const XManager* BDD_manager(const BDD* this_ptr);
+
+// In C we can not overload operators, so we use functions instead.
+BDD BDD_Operator_Not       (const BDD* this_ptr); // !
+bool BDD_Operator_Equal    (const BDD* this_ptr, const BDD* other); // ==
+BDD BDD_Operator_Or        (const BDD* this_ptr, const BDD* other); // +
+BDD BDD_Operator_And       (const BDD* this_ptr, const BDD* other); // *
+BDD BDD_Operator_Xor       (const BDD* this_ptr, const BDD* other); // ^
+BDD BDD_Operator_LessThan  (const BDD* this_ptr, const BDD* other); // <
+BDD BDD_Operator_MoreThan  (const BDD* this_ptr, const BDD* other); // >
+BDD BDD_Operator_LessEqual (const BDD* this_ptr, const BDD* other);// <=
+BDD BDD_Operator_MoreEqual (const BDD* this_ptr, const BDD* other);// >=
+BDD BDD_Operator_Nor       (const BDD* this_ptr, const BDD* other); // %
+BDD BDD_Operator_Nand      (const BDD* this_ptr, const BDD* other); // |
+BDD BDD_Operator_XNor      (const BDD* this_ptr, const BDD* other); // &
+
+DD BDD_Node(const BDD* this_ptr);
+
+XBDDManager* XBDDManager_new(int varCount);
+void XBDDManager_delete(XBDDManager* this_ptr);
+void XBDDManager_Clear(XBDDManager* this_ptr, int varCnt);
+BDD XBDDManager_BddOne(XBDDManager* this_ptr);
+BDD XBDDManager_BddZero(XBDDManager* this_ptr);
+BDD XBDDManager_BddVar(XBDDManager* this_ptr, int varIndex);
+BDD XBDDManager_Ite(XBDDManager* this_ptr, const BDD* f, const BDD* g, const BDD* h);
+
+const XManager* XBDDManager_manager(const XBDDManager* this_ptr);
+
+void XBDDManager_ShowInfo(XBDDManager* this_ptr, double vtime);
+
+}
